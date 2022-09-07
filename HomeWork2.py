7c41166425a36520456e4a625c381df3a824c201
Elena Savinykh <gudulina@mail.ru>
2022-09-07 20:17:38 +0700

    Add task 1

--- HomeWork2.py
+++ HomeWork2.py
@@ -2,15 +2,20 @@
 # Пример:   6782 -> 23
 #           0.56 -> 11
 
-num = float (input('Введите число: '))
+num = (input('Введите число: '))
+sum = 0
+for i in num:
+    if i != '.':
+        sum += int(i)
+print('Сумма цифр = ', sum)
 
 # def summa (num):
 #     if num > 0:
 #         sum = sum + num % 10
 #         num /= 10
 #     return sum
-sum = 0
-while num > 0:
-    sum = sum + num % 10
-    num /= 10
-print('Сумма цифр = ', "{:.0f}".format(sum))
+# sum = 0
+# while num > 0:
+#     sum = sum + num % 10
+#     num /= 10
+# print('Сумма цифр = ', "{:.0f}".format(sum))
