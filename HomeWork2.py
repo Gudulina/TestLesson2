75589dece51878d959733646ac89bd1554800252
Elena Savinykh <gudulina@mail.ru>
2022-09-09 23:50:29 +0700

    Add task 5

--- HomeWork2.py
+++ HomeWork2.py
@@ -23,30 +23,30 @@
 # 3. Задайте список из n чисел последовательности (1+1/N)^n и выведите на экран их сумму.
 
 # n = int (input('Введите число: '))
-# sum = 0 
-# for i in range(1, n + 1): 
-#     sum += ((1 + 1 / i)**i) 
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += ((1 + 1 / i)**i)
 # print("%.2f" % sum)
 
 # 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементво на указанных поизциях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-n = int(input("N = "))
-
-a = [i for i in range(-n, n+1)]
-print(a)
-
-mult = 1
-with open('file.txt') as file:
-    for i in file:
-        if int(i) < n:
-            mult *= a[int(i)]
-        else: mult *= 1
-print(mult)
-
-
-# for i in range(number):
-# numbers.append(randint(number * -1, number + 1))
+# n = int(input("N = "))
+# a = [i for i in range(-n, n+1)]
+# print(a)
+# mult = 1
+# with open('file.txt') as file:
+#     for i in file:
+#         if int(i) < n:
+#             mult *= a[int(i)]
+#         else: mult *= 1
+# print(mult)
 
 # 5. Реализуйте алгоритм перемешивания списка.
+
+import random
+list = random.sample(range(10), 5)
+print(f"{list} ==>")
+random.shuffle(list)
+print(list)
